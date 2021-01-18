c59612c0be51f0bc90129940b5122291794a3e65
jacopoprivato <j.progettiprivati@gmail.com>
2021-01-18 15:48:02 +0100

    movement

---
+++ yield first/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+      
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            transform.Translate(Vector3.left * 3 * Time.deltaTime);
+        }
+    }
+}
